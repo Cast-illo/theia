5a383d64bcc1c0d4754aaa1cd8558b50b1199bb6
Cast-illo <jmcast781@gmail.com>
2020-10-24 01:12:46 +0800

    commit

---
+++ story.php
@@ -0,0 +1,59 @@
+
+<html width="100%">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="styles.css">
+</head>
+<!--body------------------------------------------------see i like a girl-->
+<body id="body">
+
+
+
+	<div>
+		<!--image of theia-->
+		<img id="theia" src="images/theia4.png">
+
+		<!--THIS DIV IS FOR FUCKING HEADER-------------------------------cute and nice and tall-->
+		<div id="header">
+		</div>
+	</div>
+
+	<!--THIS DIV IS FOR FUCKING BUTTON---------------------------------but theres one problem-->
+	<div class="btn">
+		<button class="btnmenu">Home</button>
+		<button class="btnmenu">About</button>
+		<button class="btnmenu">Story</button>
+		<button class="btnmenu">Contact</button>
+	</div>
+
+	<div id="section2">
+		<img id="scroll" src="images/scroll16.png">
+			<p id="whoistheia">
+				Theia was a Titan goddess, more <br>
+				commonly referred to as the <br>
+				goddess of shining elements. <br>
+				She was associated with <br>
+				shining metals, shining <br>
+				jewels or shining light. <br>
+				She was also known as the <br>
+				goddess of sight and the <br>
+				Greeks believed her eyes <br>
+				were beams of light, helping <br>
+				them see with their own mortal <br>
+				eyes. She was one of the <br>
+				twelve Titans and like her <br>
+				famous sisters Phoebe and <br>
+				Themis, she was also associated <br>
+				with the gift of prophecy <br>
+				and had a shrine in Thessaly.
+			</p>
+	</div>
+	
+	<div id="section3">
+	</div>
+	
+	
+
+
+</body>
+</html>
